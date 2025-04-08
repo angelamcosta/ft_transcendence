be7fa0f51465bd76cebd4cd0ab8d1734759327dc
Angela Lima <angelamcostalima@icloud.com>
2025-04-08 14:22:08 +0100

    ⚡️ update `server` && `user` microservices
    - `subject` only specifies that we need to use typescript on the frontend, so `server` && `user` && other microservices will be writen in pure JavaScript

---
+++ srcs/user/tools/app.mjs
@@ -0,0 +1,20 @@
+import Fastify from 'fastify/fastify.js'
+
+const app = Fastify({ logger: true })
+const PORT = 8000
+
+const listeners = ['SIGINT', 'SIGTERM']
+listeners.forEach((signal) => {
+	process.on(signal, async () => {
+		await app.close()
+		process.exit(0)
+	})
+})
+
+app.get('/', async function handler(request, reply) {
+	return { message: 'User api is up and running' }
+})
+
+app.listen({ port: PORT, host: '0.0.0.0' }, (err) => {
+	err ? (console.error(err), process.exit(1)) : console.log(`Server running on ${PORT} 3000`)
+})
