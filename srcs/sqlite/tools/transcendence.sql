b78adb88350e2cf0921edd6ad866f08528030d5c
Miguel Diogo <miguel.g.diogo@gmail.com>
2025-04-08 19:15:03 +0100

    🔥 remove `username` field from `db`
    - also added salt field for password security/verification

--- srcs/sqlite/tools/transcendence.sql
+++ srcs/sqlite/tools/transcendence.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL,
     passwordHash TEXT NOT NULL,
+	salt TEXT NOT NULL,
     display_name TEXT UNIQUE,
     avatar TEXT DEFAULT 'default.png'
 );
