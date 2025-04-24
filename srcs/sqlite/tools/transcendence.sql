c00e91d156b75c9e13b44ce98bb9f51196f376b9
Angela Lima <angelamcostalima@icloud.com>
2025-04-24 23:10:11 +0100

    🗃️ update database schema
    * add friends table to database

--- srcs/sqlite/tools/transcendence.sql
+++ srcs/sqlite/tools/transcendence.sql
@@ -1,8 +1,17 @@
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
     passwordHash TEXT NOT NULL,
 	salt TEXT NOT NULL,
     display_name TEXT UNIQUE,
     avatar TEXT DEFAULT 'default.png'
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    user_id INTEGER,
+    friend_id INTEGER,
+    friendship_status TEXT,
+    PRIMARY KEY (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (friend_id) REFERENCES users(id)
 );
