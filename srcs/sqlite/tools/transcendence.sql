a3d1135ae643477edc6561b26aaefad4a1b83c1f
Angela Lima <angelamcostalima@icloud.com>
2025-04-30 15:17:20 +0100

    🗃️ update `database` schema

--- srcs/sqlite/tools/transcendence.sql
+++ srcs/sqlite/tools/transcendence.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS friends (
-    user_id INTEGER,
-    friend_id INTEGER,
-    friendship_status TEXT,
+    user_id INTEGER NOT NULL,
+    friend_id INTEGER NOT NULL,
+    friendship_status TEXT NOT NULL DEFAULT 'pending',
     PRIMARY KEY (user_id, friend_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (friend_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES users(id) ON DELETE CASCADE
 );
