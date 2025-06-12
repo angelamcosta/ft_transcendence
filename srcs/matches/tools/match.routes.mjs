be03db9245ce0d3974d19c74f755add582fcd6f1
Angela Lima <angela.lima@gaspardbruno.com>
2025-06-12 22:36:09 +0100

    ✨ add tournaments feature

--- srcs/matches/tools/match.routes.mjs
+++ srcs/matches/tools/match.routes.mjs
@@ -1,4 +1,4 @@
-import { autoPairPlayers, db } from './utils.mjs'
+import { db, autoPairPlayers } from './utils.mjs'
 
 export default async function matchRoutes(fastify) {
 	const interval = setInterval(() => { autoPairPlayers(fastify); }, 15000);
@@ -20,7 +20,7 @@ export default async function matchRoutes(fastify) {
 
 	fastify.get('/tournaments', async (req, res) => {
 		try {
-			const tournaments = await db.all('SELECT id, name, status FROM tournaments');
+			const tournaments = await db.all('SELECT id, name, status, capacity FROM tournaments');
 			return res.send(tournaments);
 		} catch (err) {
 			fastify.log.error(`Database error: ${err.message}`);
@@ -30,12 +30,15 @@ export default async function matchRoutes(fastify) {
 
 	fastify.post('/tournaments', async (req) => {
 		try {
-			const { name } = req.body;
+			const { name, capacity } = req.body;
 
 			if (!name || name === undefined || typeof (name) !== 'string')
 				throw fastify.httpErrors.unprocessableEntity('Request body is required');
 
-			await db.run('INSERT INTO tournaments (name) VALUES (?)', [name]);
+			if (typeof capacity !== 'number' || capacity <= 0 || (capacity & (capacity - 1)) !== 0 || 6 >= capacity)
+				throw fastify.httpErrors.unprocessableEntity('`capacity` must be a power of two and has a max of 6 players');
+
+			await db.run('INSERT INTO tournaments (name, capacity) VALUES (?, ?)', [name, capacity]);
 			return { message: 'Tournament created successfully' };
 		} catch (err) {
 			fastify.log.error(`Database error: ${err.message}`);
@@ -44,87 +47,80 @@ export default async function matchRoutes(fastify) {
 	});
 
 	fastify.post('/tournaments/:id/players', {
-		preValidation: fastify.loadTournament,
+		preValidation: fastify.loadTournament
 	}, async (req) => {
-		try {
-			const { user_id } = req.body;
+		const { user_id } = req.body;
+		const tour = req.tournament;
 
-			if (!user_id || user_id === undefined)
-				throw fastify.httpErrors.unprocessableEntity('Request body is required');
+		if (!user_id)
+			throw fastify.httpErrors.unprocessableEntity('`user_id` is required');
 
-			if (req.tournament.status !== 'open')
-				throw fastify.httpErrors.forbidden('Tournament is not accepting new players');
+		if (tour.status !== 'open')
+			throw fastify.httpErrors.forbidden('Tournament not open for registration');
 
-			const existing = await db.get('SELECT id FROM players WHERE tournament_id = ? and user_id = ?', [req.tournament.id, user_id]);
+		const exists = await db.get('SELECT 1 FROM players WHERE tournament_id = ? AND user_id = ?', [tour.id, user_id]);
 
-			if (existing)
-				throw fastify.httpErrors.conflict('Player already registered in this tournament');
+		if (exists)
+			throw fastify.httpErrors.conflict('Already joined');
 
-			await db.run('INSERT INTO players (tournament_id, user_id) VALUES (?, ?)', [req.tournament.id, user_id]);
-			return { message: 'Player added to tournament successfully' };
-		} catch (err) {
-			fastify.log.error(`Database error: ${err.message}`);
-			throw fastify.httpErrors.internalServerError('Database update failed: ' + err.message);
-		}
+		await db.run('INSERT INTO players (tournament_id, user_id) VALUES (?, ?)', [tour.id, user_id]);
+		const { count } = await db.get('SELECT COUNT(*) AS count FROM players WHERE tournament_id = ?', [tour.id]);
+
+		if (count === tour.capacity)
+			// TODO : - start tounament
+		return { message: 'Joined tournament', players: count };
 	});
 
 	fastify.get('/tournaments/:id/matches', {
-		preValidation: fastify.loadTournament,
+		preValidation: fastify.loadTournament
 	}, async (req) => {
-		try {
-			const matches = await db.all('SELECT m.id, p1.id AS player1_id, u1.display_name AS player1_name, p2.id AS player2_id, u2.display_name AS player2_name, m.status, m.score, m.created_at FROM matches m LEFT JOIN players p1 ON m.player1_id = p1.id LEFT JOIN users u1 ON p1.user_id = u1.id LEFT JOIN players p2 ON m.player2_id = p2.id LEFT JOIN users u2 ON p2.user_id = u2.id WHERE m.tournament_id = ? ORDER BY m.created_at DESC', [req.tournament.id]);
-			return { matches };
-		} catch (err) {
-			fastify.log.error(`Database error: ${err.message}`);
-			throw fastify.httpErrors.internalServerError('Database update failed: ' + err.message);
-		}
+		const matches = await db.all(`SELECT m.id, p1.id AS player1_id, u1.display_name AS player1, p2.id AS player2_id, u2.display_name AS player2, m.status, m.score, m.round FROM matches m JOIN players p1 ON m.player1_id = p1.id JOIN users u1 ON p1.user_id = u1.id JOIN players p2 ON m.player2_id = p2.id JOIN users u2 ON p2.user_id = u2.id WHERE m.tournament_id = ? ORDER BY m.round, m.created_at`, [req.tournament.id]);
+
+		return { matches };
 	});
 
 	fastify.post('/matches/:id/result', {
-		preValidation: fastify.loadMatch,
+		preValidation: fastify.loadMatch
 	}, async (req) => {
-		try {
-			const winnerId = Number(req.body.winner_id);
-			const { score } = req.body;
-			const match = req.match;
+		const winnerId = Number(req.body.winner_id);
+		const score = req.body.score;
+		const match = req.match;
 
-			if (![match.player1_id, match.player2_id].includes(winnerId))
-				throw fastify.httpErrors.unprocessableEntity('Invalid winnerId: not a player in this match');
+		if (![match.player1_id, match.player2_id].includes(winnerId))
+			throw fastify.httpErrors.unprocessableEntity('Invalid winner');
+		if (match.status === 'finished')
+			throw fastify.httpErrors.conflict('Match already finished');
 
-			if (typeof winnerId !== 'number' || typeof score !== 'string')
-				throw fastify.httpErrors.unprocessableEntity('winnerId and score are required');
+		await db.run('UPDATE matches SET winner_id=?, score=?, status=?, updated_at=CURRENT_TIMESTAMP WHERE id=?', [winnerId, score, 'finished', match.id]);
 
-			if (match.status === 'finished')
-				throw fastify.httpErrors.conflict('Match is already finished');
+		const loser = winnerId === match.player1_id ? match.player2_id : match.player1_id;
 
-			const loserId = winnerId === match.player1_id ? match.player2_id : match.player1_id;
+		await db.run('UPDATE players SET status=? WHERE id IN (?, ?)', ['winner', winnerId, loser]);
+		await db.run('UPDATE players SET status=? WHERE id=?', ['loser', loser]);
 
-			await db.run('UPDATE matches SET winner_id = ?, score = ?, status = ?, updated_at = CURRENT_TIMESTAMP  WHERE id = ?', [winnerId, score, 'finished', match.id]);
+		// TODO : - advance round if in tournament
 
-			await db.run('UPDATE players SET status = ? WHERE id = ?', ['winner', winnerId]);
-			await db.run('UPDATE players SET status = ? WHERE id = ?', ['loser', loserId]);
-
-			return { "success": true };
-		} catch (err) {
-			fastify.log.error(`Database error: ${err.message}`);
-			throw fastify.httpErrors.internalServerError('Database update failed: ' + err.message);
-		}
+		return { success: true };
 	});
 
-	fastify.get('/matches/:id', async (req) => {
-		try {
-			const match = await db.get('SELECT m.*, p1.id AS player1_id, u1.display_name AS player1_name, p2.id AS player2_id, u2.display_name AS player2_name, w.id AS winner_id, uw.display_name AS winner_name FROM matches m LEFT JOIN players p1 ON m.player1_id = p1.id LEFT JOIN users u1 ON p1.user_id = u1.id LEFT JOIN players p2 ON m.player2_id = p2.id LEFT JOIN users u2 ON p2.user_id = u2.id LEFT JOIN players w ON m.winner_id = w.id LEFT JOIN users uw ON w.user_id = uw.id WHERE m.id = ?', [req.params.id]);
-			return { match };
-		} catch (err) {
-			fastify.log.error(`Database error: ${err.message}`);
-			throw fastify.httpErrors.internalServerError('Database update failed: ' + err.message);
-		}
-	});
+	fastify.get('/matches/:id',
+		{ preValidation: fastify.loadMatch },
+		async (req) => {
+			try {
+				const match = await db.get('SELECT m.*, p1.id AS player1_id, u1.display_name AS player1_name, p2.id AS player2_id, u2.display_name AS player2_name, w.id AS winner_id, uw.display_name AS winner_name FROM matches m LEFT JOIN players p1 ON m.player1_id = p1.id LEFT JOIN users u1 ON p1.user_id = u1.id LEFT JOIN players p2 ON m.player2_id = p2.id LEFT JOIN users u2 ON p2.user_id = u2.id LEFT JOIN players w ON m.winner_id = w.id LEFT JOIN users uw ON w.user_id = uw.id WHERE m.id = ?', [req.params.id]);
+				return { match };
+			} catch (err) {
+				fastify.log.error(`Database error: ${err.message}`);
+				throw fastify.httpErrors.internalServerError('Database update failed: ' + err.message);
+			}
+		});
 
 	// ! matchmaking
 	fastify.delete('/matchmaking/leave', async (req, reply) => {
-		const { user_id } = req.body?.user_id;
-		if (!user_id) throw fastify.httpErrors.un
+		const { user_id } = req.body;
+
+		if (!user_id) throw fastify.httpErrors.unprocessableEntity('`user_id` is required');
+
 		try {
 			const result = await db.run(`DELETE FROM matchmaking_queue WHERE player_id = ?`, [user_id]);
 			return { "left": result.changes > 0 };
